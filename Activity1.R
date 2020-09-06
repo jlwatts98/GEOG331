4f206252f839681d47ae025e7e3fbd4defc6932e
jlwatts98 <jwatts@colgate.edu>
2020-09-05 21:55:48 -0400

    Finishing up Activity 1. Learning vectors and basic calculations.

--- Activity1.R
+++ Activity1.R
@@ -1,1 +1,25 @@
+##### Activity 1 - Environmental Data Science #####
 print("Hello World")
+
+# anything that is after a hastag (#) will be ignored by the computer
+
+# it is very important to annotate your code to provide your future self and other scientists
+# what exactly you were doing and why
+
+##### Calculations #####
+6^6
+238 +4
+15-23
+# naming a number
+a <- 6349
+# multiply the number by 6
+a *6
+# divide the number by 3
+a/3
+
+# vector
+b <- c(234, 456, 789, 879)
+#divide all numbers by 2
+b/2
+b*7
+b^3
